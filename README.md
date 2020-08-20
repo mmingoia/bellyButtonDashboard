e4e6637398fd07750640ab5ad5929386383ee7f4
mmingoia <michaelmingoia77@gmail.com>
2020-08-20 14:32:05 -0500

    challenge complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bellyButtonDashboard
+
 UTDA Module 12 Challenge
+
+## Link to Deployed website
+
+https://mmingoia.github.io/bellyButtonDashboard/
--- plots.js
+++ plots.js
@@ -17,11 +17,14 @@ function init() {
 function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
-  }
+}
+
+
 
 
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
+      console.log(data);
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
@@ -30,11 +33,95 @@ function buildMetadata(sample) {
       PANEL.html("");
       
       PANEL.append("h6").text("ID: " + result.id);
-      PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
-      PANEL.append("h6").text("GENDER: " + result.gender);
-      PANEL.append("h6").text("AGE: " + result.age);
-      PANEL.append("h6").text("LOCATION: " + result.location);
-      PANEL.append("h6").text("BBTYPE: " + result.bbtype);
-      PANEL.append("h6").text("WFREQ: " + result.wfreq);
+      PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
+      PANEL.append("h6").text("Gender: " + result.gender);
+      PANEL.append("h6").text("Age: " + result.age);
+      PANEL.append("h6").text("Location: " + result.location);
+      PANEL.append("h6").text("BB Type: " + result.bbtype);
+      PANEL.append("h6").text("WFreq: " + result.wfreq);
     });
 }
+
+function buildCharts(sample){
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => parseInt(sampleObj.id) == sample);
+    var result = resultArray[0];
+    var xaxis = result.sample_values.splice(0,10);
+    var yaxis = result.otu_ids.splice(0,10);;
+    for(var i=0;i<yaxis.length;i++){
+      yaxis[i] = "OTU "+ yaxis[i];
+    }
+    var text = result.otu_labels.splice(0,10);;
+    
+    var bardata = {
+    x: xaxis,
+    y: yaxis,
+    type: 'bar',
+    hovertext: text,
+    orientation: 'h'
+   };
+   
+   var barlayout = {
+    title: "Bar Chart of Top 10 Samples",
+    yaxis:{autorange:'reversed'}
+   };
+   Plotly.newPlot("bar", [bardata], barlayout);
+
+  });
+
+
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => parseInt(sampleObj.id) == sample);
+    var result = resultArray[0];
+    
+    var sampleValues = result.sample_values;
+    var otuIDs = result.otu_ids;
+    var otuLabels = result.otu_labels;
+    var bubbleData = {
+    x: otuIDs,
+    y: sampleValues,
+    text: otuLabels,
+    mode: 'markers',
+    marker: {
+      size: sampleValues,
+      sizeref: 1.5,
+      color : otuIDs
+    }
+        
+   }
+   var bubbleLayout = {
+    title: "Bubble Chart of all bacteria",
+  };
+  Plotly.newPlot("bubble", [bubbleData], bubbleLayout);
+  });
+
+
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var wash = result.wfreq;
+    
+    
+    var gaugeData = [
+      {
+        value: wash,
+        type : "indicator",
+        gauge: {
+          axis:{range: [0,9]},
+        },
+        mode : 'gauge'
+
+      }
+    ];
+  var layout = {
+    width: 600,
+    height: 450,
+    margin: { t: 0, b: 0 }
+    };
+  Plotly.newPlot('gauge', gaugeData, layout)
+  });
+}
+window.onload - optionChanged(940);
